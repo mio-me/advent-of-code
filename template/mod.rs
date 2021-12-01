3cca75b528f64d51240b7b1a23a4b3b53b5fc8ff
Thorben Below <below.thorben@inside-online.de>
2021-12-01 10:54:03 +0100

    add template

---
+++ template/mod.rs
@@ -0,0 +1,33 @@
+use crate::util;
+
+pub fn solve() {
+  util::solve("d04", p1, p2)
+}
+
+fn p1(input: &str) -> usize {
+  input
+    .lines()
+    .count()
+}
+
+fn p2(input: &str) -> usize {
+  input
+    .lines()
+    .count()
+}
+
+
+#[cfg(test)]
+mod test {
+  const DATA: &str = "";
+  use super::*;
+  #[test]
+  fn s1() {
+    assert_eq!(p1(DATA), 2);
+  }
+
+  #[test]
+  fn s2() {
+    assert_eq!(p2(DATA), 4);
+  }
+}
